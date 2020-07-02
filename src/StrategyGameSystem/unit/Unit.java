c63b8811b6e5aae4b0615139c1ec265f6940d1d3
Andrzej Witkowski <endrjus8@gmail.com>
2020-07-02 22:18:11 +0200

    Change Unit class.

--- src/StrategyGameSystem/unit/Unit.java
+++ src/StrategyGameSystem/unit/Unit.java
@@ -1,8 +1,34 @@
 package StrategyGameSystem.unit;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Unit {
 
-    public Unit() {
+    private String type;
+    private Map<String, Object> properties;
 
+    public Unit(String type) {
+        this.type = type;
+        properties = new HashMap<>();
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setProperty(String propertyName, Object propertyValue) {
+        properties.put(propertyName, propertyValue);
+    }
+
+    public Object getProperty(String propertyName) {
+        return properties.get(propertyName);
     }
 }
+
